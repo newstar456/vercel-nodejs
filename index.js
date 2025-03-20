fe1591f4af93b84dd592a82d45df8d7d2ea8570e
Viktoria Vonsovych <vikvons@gmail.com>
2025-03-20 20:16:52 +0100

    vercel-server

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://eyxccahssmwxluobjzyb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5eGNjYWhzc213eGx1b2JqenliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0MTE3NDYsImV4cCI6MjA1Nzk4Nzc0Nn0.EyAezK8taMRdmnGdvmQFSI2wtbEhFqlsBjE8n4NQIu4')
+
+async function createConnection() {
+  // const supabase = createClient();
+  try {
+    const {data, error} = await supabase.from('Shops').select();
+    console.log(data);
+    console.log('Connection to SUPABASE has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+createConnection();
+
+
+
+
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 });
